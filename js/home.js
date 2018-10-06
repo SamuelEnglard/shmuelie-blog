2a3688886889950e3203bdedab91bdadef670ffe
Shmueli Englard <shmueli.yosef@englard.net>
2018-10-06 12:06:14 -0400

    Added beginning of home page content

--- js/home.js
+++ js/home.js
@@ -4,7 +4,11 @@ define(["require", "exports", "winjs"], function (require, exports, WinJS) {
     var homePageUrl = "pages/home.htm";
     WinJS.UI.Pages.define(homePageUrl, {
         ready: function (element, options) {
+            this._element = element;
             WinJS.UI.processAll(element);
+        },
+        getAnimationElements: function () {
+            return WinJS.Utilities.children(this._element).slice();
         }
     });
     exports.default = homePageUrl;
--- ts/home.ts
+++ ts/home.ts
@@ -1,12 +1,18 @@
 import * as WinJS from 'winjs'
 
 interface HomePageControl extends WinJS.UI.Pages.IPageControlMembers {
+    getAnimationElements(): Element[]
+    _element: HTMLElement
 }
 
 const homePageUrl = "pages/home.htm";
-WinJS.UI.Pages.define(homePageUrl, {
+WinJS.UI.Pages.define(homePageUrl, <WinJS.UI.Pages.IPageControlMembers>{
     ready: function (this: HomePageControl, element: HTMLElement, options: any): void {
+        this._element = element;
         WinJS.UI.processAll(element);
+    },
+    getAnimationElements: function (this: HomePageControl): Element[] {
+        return WinJS.Utilities.children(this._element).slice();
     }
 });
 export default homePageUrl;
