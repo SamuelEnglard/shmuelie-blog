0f25b115cd0369473aef8063b3b97dc744878b6b
Shmueli Englard <shmueli.yosef@englard.net>
2018-10-07 21:50:14 -0400

    Improved loading animation and made twitter widget load smoother

--- js/contact.js
+++ js/contact.js
@@ -4,9 +4,20 @@ define(["require", "exports", "winjs", "twitter", "linkedin"], function (require
     var contactPageUrl = "pages/contact.htm";
     WinJS.UI.Pages.define(contactPageUrl, {
         ready: function (element, options) {
-            twttr.widgets.load(element);
+            this._element = element;
+            var p = new WinJS.Promise(function (completeDispatch) {
+                twttr.ready(function () {
+                    twttr.events.bind("loaded", function (ev) {
+                        completeDispatch(ev);
+                    });
+                    twttr.widgets.load(element);
+                });
+            });
             IN.parse(element);
-            WinJS.UI.processAll(element);
+            return p;
+        },
+        getAnimationElements: function () {
+            return WinJS.Utilities.children(this._element).slice();
         }
     });
     exports.default = contactPageUrl;
--- ts/contact.ts
+++ ts/contact.ts
@@ -2,16 +2,32 @@ import * as WinJS from 'winjs'
 import 'twitter'
 import 'linkedin'
 
+interface ContactPageControl extends WinJS.UI.Pages.IPageControlMembers {
+    getAnimationElements(): Element[]
+    _element: HTMLElement
+}
+
 declare const IN: {
     parse: (element: HTMLElement) => void
 };
 
 const contactPageUrl = "pages/contact.htm";
-WinJS.UI.Pages.define(contactPageUrl, {
-    ready: function (this: WinJS.UI.Pages.IPageControlMembers, element: HTMLElement, options: any): void {
-        twttr.widgets.load(element);
+WinJS.UI.Pages.define(contactPageUrl, <WinJS.UI.Pages.IPageControlMembers>{
+    ready: function (this: ContactPageControl, element: HTMLElement, options: any): WinJS.Promise<void> {
+        this._element = element;
+        let p = new WinJS.Promise<void>(function (completeDispatch) {
+            twttr.ready(function () {
+                twttr.events.bind("loaded", function (ev) {
+                    completeDispatch(ev);
+                });
+                twttr.widgets.load(element);
+            });
+        });
         IN.parse(element);
-        WinJS.UI.processAll(element);
+        return p;
+    },
+    getAnimationElements: function (this: ContactPageControl): Element[] {
+        return WinJS.Utilities.children(this._element).slice();
     }
 });
 export default contactPageUrl;
