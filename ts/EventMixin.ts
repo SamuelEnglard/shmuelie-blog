a4dee1444f21227194746f26db36a66f6c967f99
Shmueli Englard <shmueli.yosef@englard.net>
2018-10-15 20:49:50 -0400

    Added documentation and better type info to EventMixin

--- ts/EventMixin.ts
+++ ts/EventMixin.ts
@@ -1,8 +1,26 @@
 import * as WinJS from 'winjs'
 
 declare class EventMixin {
+    /**
+     * Adds an event listener to the control.
+     * @param type The type (name) of the event.
+     * @param listener The listener to invoke when the event gets raised.
+     * @param useCapture If true, initiates capture, otherwise false.
+    **/
     addEventListener(type: string, listener: Function, useCapture?: boolean): void;
-    dispatchEvent(type: string, eventProperties: any): boolean;
+    /**
+     * Raises an event of the specified type and with the specified additional properties.
+     * @param type The type (name) of the event.
+     * @param eventProperties The set of additional properties to be attached to the event object when the event is raised.
+     * @returns true if preventDefault was called on the event.
+    **/
+    protected dispatchEvent(type: string, eventProperties: any): boolean;
+    /**
+     * Removes an event listener from the control.
+     * @param type The type (name) of the event.
+     * @param listener The listener to remove.
+     * @param useCapture true if capture is to be initiated, otherwise false.
+    **/
     removeEventListener(type: string, listener: Function, useCapture?: boolean): void;
 }
 const EventMixin: EventMixin = <EventMixin><any>function () { };
