5da003cb6a0b2741c00eea3398b403c05f771116
Shmueli Englard <shmueli.yosef@englard.net>
2018-10-18 22:58:19 -0400

    Added basic blog UI

--- js/blog.js
+++ js/blog.js
@@ -2,7 +2,7 @@ define(["require", "exports", "winjs", "stateManager"], function (require, expor
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var posts = new WinJS.Binding.List([
-        { name: "Rocco Gower", icon: "images/people/person1.png", time: "8:05p", messageText: "Did you get your tickets yet?", url: "posts/a.htm" },
+        { name: "HoloLens", icon: "images/people/person1.png", time: "8:05p", messageText: "Hololens at Build 2015", url: "posts/a.htm" },
         { name: "Alonzo Swope", icon: "images/people/person5.png", time: "7:34p", messageText: "I think we're all set. See you at the meeting tomorrow!", url: "posts/b.htm" },
         { name: "Heather Richmond", icon: "images/people/person7.png", time: "7:30p", messageText: "Let's schedule some time to review the latest reports.", url: "posts/c.htm" }
     ]);
@@ -16,8 +16,12 @@ define(["require", "exports", "winjs", "stateManager"], function (require, expor
                 WinJS.Navigation.navigate("#blog://" + posts.getAt(e.detail.itemIndex).url);
             });
             var user = StateManager.register("blog");
+            var contentArea = element.querySelector(".win-splitview-content");
             user.addEventListener("navigated", function (e) {
-                console.log(postByUrl.groups.getItemFromKey(e.detail.location).data.messageText);
+                var selectedPost = postByUrl.groups.getItemFromKey(e.detail.location).data;
+                postList.selection.set(posts.indexOf(selectedPost));
+                contentArea.innerHTML = "";
+                WinJS.UI.Fragments.renderCopy(e.detail.location, contentArea);
             });
         }
     });
--- ts/blog.ts
+++ ts/blog.ts
@@ -10,7 +10,7 @@ interface BlogEntry {
 }
 
 const posts = new WinJS.Binding.List<BlogEntry>([
-    { name: "Rocco Gower", icon: "images/people/person1.png", time: "8:05p", messageText: "Did you get your tickets yet?", url: "posts/a.htm" },
+    { name: "HoloLens", icon: "images/people/person1.png", time: "8:05p", messageText: "Hololens at Build 2015", url: "posts/a.htm" },
     { name: "Alonzo Swope", icon: "images/people/person5.png", time: "7:34p", messageText: "I think we're all set. See you at the meeting tomorrow!", url: "posts/b.htm" },
     { name: "Heather Richmond", icon: "images/people/person7.png", time: "7:30p", messageText: "Let's schedule some time to review the latest reports.", url: "posts/c.htm" }
 ]);
@@ -25,8 +25,12 @@ WinJS.UI.Pages.define("pages/blog.htm", {
             WinJS.Navigation.navigate("#blog://" + posts.getAt(e.detail.itemIndex).url);
         });
         const user = StateManager.register("blog");
+        const contentArea: HTMLDivElement = <HTMLDivElement>element.querySelector(".win-splitview-content");
         user.addEventListener("navigated", (e: CustomEvent<StateManager.NavigatedDetails>) => {
-            console.log(postByUrl.groups.getItemFromKey(e.detail.location).data.messageText);
+            const selectedPost = postByUrl.groups.getItemFromKey(e.detail.location).data;
+            postList.selection.set(posts.indexOf(selectedPost));
+            contentArea.innerHTML = "";
+            WinJS.UI.Fragments.renderCopy(e.detail.location, contentArea);
         });
     }
 });
